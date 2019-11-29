12c836f6f79029a64acd303c0d3a3a2c0ac98cef
algot <algot@ua.fm>
2019-11-29 16:10:35 +0200

    Fix syntax highlight

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,9 @@
-def test_guest_can_go_to_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
+from selenium.webdriver import Remote
+
+link = 'http://selenium1py.pythonanywhere.com/'
+
+
+def test_guest_can_go_to_login_page(browser: Remote):
     browser.get(link)
-    login_link = browser.find_element_by_css_selector('#login_link')
-    login_link.click()
+    go_to_login_page(browser)
+
