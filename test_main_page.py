ee46c85699b0ce6ff13437392ec8901bcc122bf6
algot <algot@ua.fm>
2019-11-29 16:42:15 +0200

    Implement is_element_present

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,16 @@
-from selenium.webdriver import Remote
+from selenium.webdriver.remote import webdriver
 from pages.main_page import MainPage
 
 link = 'http://selenium1py.pythonanywhere.com/'
 
 
-def test_guest_can_go_to_login_page(browser: Remote):
+def test_guest_can_go_to_login_page(browser: webdriver):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser: webdriver):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
