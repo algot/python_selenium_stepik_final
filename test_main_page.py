2262df7ff96c7e6843e38e579d34baf7fe9253bc
algot <algot@ua.fm>
2019-11-29 16:13:20 +0200

    First page object implementation

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,10 @@
 from selenium.webdriver import Remote
+from pages.main_page import MainPage
 
 link = 'http://selenium1py.pythonanywhere.com/'
 
 
 def test_guest_can_go_to_login_page(browser: Remote):
-    browser.get(link)
-    go_to_login_page(browser)
-
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
