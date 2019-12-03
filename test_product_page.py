7888a1b70c63c77d7073976b837bda4ad9422782
algot <algot@ua.fm>
2019-12-03 16:52:14 +0200

    Final commit

--- test_product_page.py
+++ test_product_page.py
@@ -21,6 +21,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
                               marks=pytest.mark.xfail),
                           "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                           "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, promo_link):
     product_page = ProductPage(browser, promo_link)
     product_page.open()
@@ -52,6 +53,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link_city95 = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
     page = ProductPage(browser, link_city95)
@@ -61,6 +63,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link_coders207 = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link_coders207)
@@ -89,6 +92,7 @@ class TestUserAddToBasketFromProductPage:
         product_page.open()
         product_page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         product_page = ProductPage(browser, link)
         product_page.open()
